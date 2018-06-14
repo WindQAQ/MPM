1409776f556add9899b8d8a9d3d0d1afe9fd180a
yistLin <yishen992@gmail.com>
2018-06-14 20:02:45 +0000

    partially follow the configurations in paper

--- inc/constant.h
+++ inc/constant.h
@@ -2,26 +2,28 @@
 #define CONSTANT_H_
 
 #define TIMESTEP 1e-3
-#define HARDENING 5.0f
+#define HARDENING 10.0f
 #define GRAVITY -9.8f
-#define CRIT_COMPRESS (1 - 1.9e-2)
+#define CRIT_COMPRESS (1 - 2.5e-2)
 #define CRIT_STRETCH (1 + 7.5e-3)
-#define ALPHA 0.95f
+#define ALPHA 0.92f
 #define PARTICLE_DIAM 0.01f
 #define STICKY_WALL 0.9f
 #define FRICTION 1.0f
 
-#define YOUNG 4.8e4f
+#define YOUNG 1.4e5f
 #define POISSON 0.2f
 
-#define LAMBDA ((POISSON * YOUNG)) / ((1.0f + POISSON) * (1.0f - 2.0f * POISSON))
+#define LAMBDA ((POISSON * YOUNG) / ((1.0f + POISSON) * (1.0f - 2.0f * POISSON)))
 #define MU (YOUNG / (2.0f * (1.0f + POISSON)))
 
 #define GRID_BOUND_X 100
 #define GRID_BOUND_Y 100
 #define GRID_BOUND_Z 100
 
-#define BOX_BOUNDARY_1 ((0 + 5) * PARTICLE_DIAM)
-#define BOX_BOUNDARY_2 ((GRID_BOUND_X - 5) * PARTICLE_DIAM)
+#define BOX_BOUNDARY_1 (0.0 * PARTICLE_DIAM)
+#define BOX_BOUNDARY_2 (100.0 * PARTICLE_DIAM)
+
+#define G2P 2
 
 #endif  // CONSTANT_H_

250d08b2fe7cccbe07000600a24c460eda15ee95
WindQAQ <windqaq@gmail.com>
2018-06-18 01:32:47 +0000

    fix typo

--- inc/linalg.h
+++ inc/linalg.h
@@ -9,7 +9,7 @@ namespace linalg {
     __host__ __device__ void svd3(const Eigen::Matrix3f& a, Eigen::Matrix3f& u, Eigen::Matrix3f& s, Eigen::Matrix3f& v) {
         svd(a(0), a(3), a(6), a(1), a(4), a(7), a(2), a(5), a(8),
             u(0), u(3), u(6), u(1), u(4), u(7), u(2), u(5), u(8),
-            s(0), s(3), u(6), s(1), s(4), s(7), s(2), s(5), s(8),
+            s(0), s(3), s(6), s(1), s(4), s(7), s(2), s(5), s(8),
             v(0), v(3), v(6), v(1), v(4), v(7), v(2), v(5), v(8));
     }
 

17a493cd3194d2b6f0e511c363bfeb746ee0c51c
WindQAQ <windqaq@gmail.com>
2018-06-19 12:59:35 +0000

    remove unused constants

--- inc/constant.h
+++ inc/constant.h
@@ -1,28 +1,30 @@
 #ifndef CONSTANT_H_
 #define CONSTANT_H_
 
-#define TIMESTEP 1e-3
-#define HARDENING 10.0f
-#define GRAVITY -9.8f
-#define CRIT_COMPRESS (1 - 2.5e-2)
-#define CRIT_STRETCH (1 + 7.5e-3)
-#define ALPHA 0.92f
-#define PARTICLE_DIAM 0.01f
+#include <Eigen/Dense>
+
+#define TIMESTEP 1e-4f
+#define GRAVITY Eigen::Vector3f(0.0f, -9.8f, 0.0f)
+#define PARTICLE_DIAM 0.005f
+
+#define ALPHA 0.95f
 #define STICKY_WALL 0.9f
 #define FRICTION 1.0f
 
-#define YOUNG 1.4e5f
-#define POISSON 0.2f
-
-#define LAMBDA ((POISSON * YOUNG) / ((1.0f + POISSON) * (1.0f - 2.0f * POISSON)))
-#define MU (YOUNG / (2.0f * (1.0f + POISSON)))
-
-#define GRID_BOUND_X 100
-#define GRID_BOUND_Y 100
-#define GRID_BOUND_Z 100
+#define GRID_BOUND_X 200
+#define GRID_BOUND_Y 200
+#define GRID_BOUND_Z 200
 
 #define BOX_BOUNDARY_1 (0.0 * PARTICLE_DIAM)
-#define BOX_BOUNDARY_2 (100.0 * PARTICLE_DIAM)
+#define BOX_BOUNDARY_2 (GRID_BOUND_X * PARTICLE_DIAM)
+
+#define ENABLE_IMPLICIT false
+
+#if ENABLE_IMPLICIT
+#define SOLVE_MAX_ITERATIONS 10
+#define RESIDUAL_THRESHOLD
+#define BETA 0.5
+#endif
 
 #define G2P 2
 
